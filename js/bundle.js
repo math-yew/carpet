eab3d91f02af81309bdb5bedb8d02493a76782ef
Matt Mecham <math.u@hotmail.com>
2017-06-15 10:55:40 -0600

    first

---
+++ js/bundle.js
@@ -0,0 +1,57 @@
+$(window).on('scroll', function() {
+
+  var scrollPixel = $(this).scrollTop();
+  // console.log(scrollPixel);
+
+  // $('.hamburger').on('hover', function () {
+  // $('.hamburger').css({'background-color':'red'});
+  // $('.nav-bar ul').css({'display':'inline'});
+  // });
+
+  $('.title').css({
+    'transform':'translateY('+ scrollPixel / 4 + '%)'
+  });
+
+  if(scrollPixel <= 100){
+  $('nav').css({'background-color':'transparent'});
+  }
+
+  if(scrollPixel > 75){
+  $('nav').css({'background-color':'#3d2e24'});
+  }
+
+  if(scrollPixel <= 75){
+  $('nav').css({'background-color':'transparent'});
+  }
+
+  if(scrollPixel > 200){
+    console.log('fade in: ');
+  $('.fade1').css({'opacity':'1'});
+  }
+
+  if(scrollPixel > 1000){
+  $('.fade1').css({'opacity':'1'});
+  }
+
+  if(scrollPixel > 600){
+  $('.slide').css({
+    'transform':'translate(0%, 0%)'
+  });
+}
+
+
+  if(scrollPixel > 1250){
+  $('.fade2').css({'opacity':'1'});
+  }
+
+
+////////end/////////
+});
+
+$('a.sect').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+});
